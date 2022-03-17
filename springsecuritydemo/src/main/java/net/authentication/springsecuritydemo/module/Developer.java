173da86a729af2b1800c495f7026ba6f33479d0d
Meiirlan Serikbay <98660924+7meiirsse@users.noreply.github.com>
2022-03-17 15:37:47 +0600

    Add files via upload
    practical examples cover the basics of working with the Spring Security Framework

---
+++ springsecuritydemo/src/main/java/net/authentication/springsecuritydemo/module/Developer.java
@@ -0,0 +1,12 @@
+package net.authentication.springsecuritydemo.module;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Developer {
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
